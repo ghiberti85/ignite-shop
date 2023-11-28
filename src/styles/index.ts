36e0cde79fd5349869573fee42cab059559af440
ghiberti85 <ghiberti85@gmail.com>
2023-11-28 14:07:20 -0300

    Stitches Configuration

---
+++ src/styles/index.ts
@@ -0,0 +1,19 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+    config,
+    createTheme,
+    css,
+    globalCss,
+    getCssText,
+    keyframes,
+    theme,
+    styled,
+} = createStitches({
+    theme: {
+        colors: {
+            rocketseat: '#8257e6',
+        }
+    }
+})
+
