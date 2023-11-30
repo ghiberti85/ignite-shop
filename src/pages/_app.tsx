33e02eb372988dc6b53ae8abdc24ad731b54d2d2
ghiberti85 <ghiberti85@gmail.com>
2023-11-29 22:17:36 -0300

    Application's Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import { globalStyle } from "@/styles/global"
 import { AppProps } from "next/app"
+import logoImg from '../assets/ignite-shop-logo.svg'
+import { Container, Header } from "@/styles/pages/app"
 
 globalStyle()
 
 export default function App({ Component, pageProps }: AppProps) {
 
 
-    return <Component {...pageProps} />
+    return (
+       <Container>
+            <Header>
+                <img src={logoImg.src} alt=""/>
+            </Header>
+
+            <Component {...pageProps} />
+       </Container>
+    )
 }
