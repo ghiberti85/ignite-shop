faef045a744a678bb351c906eaad387f656810cb
ghiberti85 <ghiberti85@gmail.com>
2023-12-01 09:42:56 -0300

    Product Page Structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,21 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
     const { query } = useRouter()
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer></ImageContainer>
+            <ProductDetails>
+                <h1> Camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid possimus a beatae, tempore odio maxime corrupti culpa tempora repellendus voluptatem cum at, vel iste nihil velit repudiandae assumenda quae accusamus.</p>
+
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
